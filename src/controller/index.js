8191003dc25c2f2aae1c3842b4d5859b52999f98
Guilherme Braz <guilhermebrazmoreirafaria@gmail.com>
2022-06-11 22:17:15 -0300

    backup

---
+++ src/controller/index.js
@@ -0,0 +1,6 @@
+import getReceitaCnpj from "./receita.controller";
+import getSituacaoCnpj from "./situacao.controller";
+import getConsultarCnpj from "./consultar.controller";
+import getCnpj from "./cnpj.controller";
+
+export { getReceitaCnpj, getSituacaoCnpj, getConsultarCnpj, getCnpj };
